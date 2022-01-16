e07605c408ecf8f9cc31337652b12fa0416953c5
Lawrencesalinas <lawrencesalinas@yahoo.com>
2022-01-15 20:40:55 -0500

    added image folder for images uploaded

--- backend/urls.py
+++ backend/urls.py
@@ -14,11 +14,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from xml.dom.minidom import Document
 from django.contrib import admin
 from django.urls import path, include
+
+from django.conf import settings
+# function that allows us to connect our urls to import static
+from django.conf.urls.static import static
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls'))
-   
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT )
+
