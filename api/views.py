c663711292d3cd7d76de46d20229a2e069a666cc
Lawrencesalinas <lawrencesalinas@yahoo.com>
2022-01-15 19:02:41 -0500

    setip the urls

---
+++ api/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .products import products
+# Create your views here.
+
+
+@api_view(['GET'])
+def getProducts(request):
+    return Response(products)
+
